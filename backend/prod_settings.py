ce1c9affb7626411a8c707ae1def2148e4d5f12d
unniznd <aanandunnis@gmail.com>
2023-05-20 08:43:32 +0530

    host added

--- backend/prod_settings.py
+++ backend/prod_settings.py
@@ -1,15 +1,19 @@
 import os
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': '',
-        'NAME': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }

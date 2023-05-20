2bbf8faeb0064ce631078b7e24aec5d36404e788
unniznd <aanandunnis@gmail.com>
2023-05-20 14:24:34 +0530

    login updated and dashboard added

--- backend/urls.py
+++ backend/urls.py
@@ -2,12 +2,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from users.views import ObtainAuthTokenWithUserId
+from users.views import ObtainAuthTokenWithUserId, GetDashboardDetails
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('get-token/', ObtainAuthTokenWithUserId.as_view()),
+    path('login/', ObtainAuthTokenWithUserId.as_view()),
+    path('dashboard/', GetDashboardDetails.as_view()),
 
     path('superuser/branch/', include('branch_class.urls'))
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Profile, BranchAdmin
 
@@ -43,6 +45,44 @@ class ObtainAuthTokenWithUserId(ObtainAuthToken):
 
         return Response({
             'token': token.key, 
+            'user_id': user.id,
+            'name':profile.name, 
+            'is_admin': profile.is_admin,
+            'is_branch_admin': profile.is_branch_admin,
+        })
+
+class GetDashboardDetails(ListAPIView):
+    permission_classes = [
+        IsAuthenticated,
+    ]
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        profile = Profile.objects.filter(user=user.id).first()
+        if(profile.is_admin):
+            branches = OfficeBranchModel.objects.all()
+            branch_serial = ViewOfficeBranchSerializer(branches, many=True)
+            
+            return Response({
+                'user_id': user.id,
+                'name':profile.name, 
+                'is_admin': profile.is_admin,
+                'is_branch_admin': profile.is_branch_admin,
+                'branches': branch_serial.data
+
+            })
+        elif(profile.is_branch_admin):
+            branch = BranchAdmin.objects.filter(admin=profile).first().branch
+            branch_serial = ViewOfficeBranchSerializer(branch)
+            return Response({
+               
+                'user_id': user.id,
+                'name':profile.name, 
+                'is_admin': profile.is_admin,
+                'is_branch_admin': profile.is_branch_admin,
+                'branches': branch_serial.data
+            })
+
+        return Response({
             'user_id': user.id,
             'name':profile.name, 
             'is_admin': profile.is_admin,
